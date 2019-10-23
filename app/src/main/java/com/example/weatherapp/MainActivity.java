d25e1721d71c8152fa744d44a93d31a71e3e3d6f
Daylon <daylonblakely@gmail.com>
2019-10-23 12:48:25 -0700

    main activity gets zipcode from user and starts a thread to get weather data. Weather data is output in activity_display_weather

--- app/src/main/java/com/example/weatherapp/MainActivity.java
+++ app/src/main/java/com/example/weatherapp/MainActivity.java
@@ -2,13 +2,41 @@ package com.example.weatherapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
+
+    public static final String EXTRA_MESSAGE = "com.example.weatherapp.MESSAGE";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    /** Called when the user taps the Submit button */
+    public void sendZip(View view) {
+        Intent intent = new Intent(this, DisplayWeather.class);
+
+        EditText zipText = findViewById(R.id.editZip);
+        String zipcode = zipText.getText().toString();
+
+        //Run network action in separate thread
+        weatherHttpReq req = new weatherHttpReq(zipcode);
+        Thread reqThread = new Thread(req);
+        reqThread.start();
+        //wait for thread to complete
+        try {
+            reqThread.join();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        String weatherData = req.getWeather();
+
+        intent.putExtra(EXTRA_MESSAGE, weatherData);
+        startActivity(intent);
     }
 }
--- app/src/main/java/com/example/weatherapp/weatherHttpReq.java
+++ app/src/main/java/com/example/weatherapp/weatherHttpReq.java
@@ -1,4 +1,55 @@
 package com.example.weatherapp;
 
-public class weatherHttpReq {
+import java.io.BufferedReader;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
+public class weatherHttpReq implements Runnable{
+    private String zipReq = "http://api.openweathermap.org/data/2.5/weather?zip=";
+    private String apiKey = "&appid=d4d40ead11f72883138f3da559e2cb94";
+    private String zipcode;
+    private String weatherData;
+
+    public weatherHttpReq(String zipcode){
+        this.zipcode = zipcode;
+    }
+
+    public String getWeather(){
+        return weatherData;
+    }
+
+    public void run(){
+        HttpURLConnection con = null ;
+        InputStream is = null;
+
+        try {
+            con = (HttpURLConnection) ( new URL(zipReq+zipcode+apiKey)).openConnection();
+            con.setRequestMethod("GET");
+            con.setDoInput(true);
+            con.setDoOutput(true);
+            con.connect();
+
+            // Let's read the response
+            StringBuffer buffer = new StringBuffer();
+            is = con.getInputStream();
+            BufferedReader br = new BufferedReader(new InputStreamReader(is));
+            String line = null;
+            while (  (line = br.readLine()) != null )
+                buffer.append(line + "\r\n");
+
+            is.close();
+            con.disconnect();
+            weatherData = buffer.toString();
+        }
+        catch(Throwable t) {
+            t.printStackTrace();
+        }
+        finally {
+            try { is.close(); } catch(Throwable t) {}
+            try { con.disconnect(); } catch(Throwable t) {}
+        }
+    }
+
 }
